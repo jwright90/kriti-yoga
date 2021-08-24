62c3ffcf89a50f063cd21623038a24094c8d3cc0
jwright90 <jason.wright90@gmail.com>
2021-08-24 12:55:43 +0100

    make progress on menu overlay for mobile

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function menuCross(divEl) {
+function addChangeClass(divEl) {
   divEl.classList.toggle("change");
+
+  let mobileOverlay;
+  mobileOverlay = document.getElementById("mobile-overlay");
+  mobileOverlay.classList.toggle("change");
+
+  // let mobileNav;
+  // mobileNav = document.getElementById("mobile-nav");
+  // mobileNav.classList.toggle("white");
 }
